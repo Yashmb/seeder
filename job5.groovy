f43165ca54f1ec88d6c33c314aeee6c0cc2ddd5e
Yashwanth <yeshmb31@gmail.com>
2020-07-09 16:23:21 +0530

    final commit

--- job5.groovy
+++ job5.groovy
@@ -1,6 +1,6 @@
 buildPipelineView('pipeline_view') {
     title('pipeline_view')
-    displayedBuilds(5)
+    displayedBuilds(1)
     selectedJob('job1')
     showPipelineParameters(true)
     refreshFrequency(5)
